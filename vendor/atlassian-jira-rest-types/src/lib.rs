bc9036528d044f7ce5043ecab7bac5ac5f31ebcb
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2024-10-04 16:24:40 +0300

    ci: Do not vendor dependencies

--- vendor/atlassian-jira-rest-types/src/lib.rs
+++
@@ -1,9 +0,0 @@
-extern crate schemafy_core;
-extern crate serde;
-extern crate serde_json;
-
-pub mod v2 {
-    use serde::{Deserialize, Serialize};
-
-    schemafy::schemafy!("src/schema-v2.json");
-}
